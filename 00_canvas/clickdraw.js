7a00eba70160755d3cb571228517af8cd8aeb7eb
Shafalil Gupta <sgupta@stuy.edu>
2019-01-31 06:43:43 -0500

    done

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -4,30 +4,38 @@ var cleS = document.getElementById("cle")
 var swi = document.getElementById("draw_state")
 var state = 0;
 
-var clear.addEventListener("click",function (e){
-  ctx.clearRect(0,0,c.width,c.height);
-});
+
+var clear = function clr_canvas() {
+    context.clearRect(0, 0, c.width, c.height)
+}
+
 
 var rectangle = function make_rect(x, y) {
+
     context.fillStyle = "#ffffff"
     context.fillRect(x, y, 100, 200)
     state = 1
 };
 
 var dot = function make_dot(x, y) {
+
     context.fillStyle = "#00ff00"
     context.beginPath()
     context.ellipse(x, y, 10, 10, 360, 0, 360)
     context.fill()
     state = 0
+    console.log("ellipse made")
+
 };
+cleS.addEventListener("click", clear )
+
 
 c.addEventListener('click', function(e) {
     if (state==0) {
-        rectangle(e.clientX, e.ClientY)
+      rectangle(e.offsetX, e.offsetY)
     }
     else{
-        dot(e.ClientX, e.ClientY)
+      dot(e.offsetX, e.offsetY)
     }
 })
 swi.addEventListener('click', function() {
